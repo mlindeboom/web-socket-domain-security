bc7f3be33c5ff6a1d6cd8b03eb6cb27196772bf6
Michael Lindeboom <michael.j.lindeboom@gmail.com>
2020-01-18 18:31:05 -0500

    Create index.js

---
+++ src/wsdsDisconnect/index.js
@@ -0,0 +1,10 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = (event, context, callback) => {
+    const connectionId = event.requestContext.connectionId;
+    addConnectionId(connectionId).then(() => { callback(null, { statusCode: 200, }) });
+}
+
+function addConnectionId(connectionId) {
+    return ddb.delete({ TableName: 'WebSocketDomainSecurity', Key: { connectionId: connectionId, }, }).promise();
+}
